e3bd92dc2fd0d52372b2166fd1c1e71390dd5677
Chertenko <kchek816@gmail.com>
2024-01-04 21:23:30 +0300

    feat: PFC

--- src/components/HomeScreens/Home.tsx
+++ src/components/HomeScreens/Home.tsx
@@ -3,16 +3,21 @@ import { View, Text, StyleSheet, KeyboardAvoidingView, TouchableWithoutFeedback,
 import { HomeHeader } from "./HomeHeader";
 import { SearchInput } from "./SearchInput";
 import { FoodCategories } from "./FoodCategories";
+import { PRHeader } from "./PopularFoodCategories/PRHeader";
+import { PopularFC } from "./PopularFoodCategories/PopularFC";
 
 export const Home = () => {
     return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
         <View style={{flex: 1, marginTop: 20}}>
-            <View style ={{     marginHorizontal: 20}}>
+            <View style ={{marginHorizontal: 20}}>
                 <HomeHeader />
                 <SearchInput />
             </View>
-            <FoodCategories />
+                <FoodCategories />
+            <View style ={{marginHorizontal: 20}}>
+                <PopularFC />
+            </View>
         </View>
     </TouchableWithoutFeedback>
     )
---
+++ src/data/FakeFoodCategories.ts
@@ -0,0 +1,18 @@
+export const FakeCategories = [
+    {
+        name: 'Noodles',
+        image: '../../assets/images/noodles.png',
+        count: 30
+    },
+    {
+        name: 'Non-veg',
+        image: 'https://i.pinimg.com/736x/01/a8/b2/01a8b20022d3ac8d1c0ad960e7b67466.jpg',
+        count: 50
+    },
+    {
+        name: 'Soup',
+        image: 'https://i.pinimg.com/564x/1f/05/9e/1f059e6f812452539be4b29fd0cfffad.jpg',
+        count: 40
+    }
+]
+
