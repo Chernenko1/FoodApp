f405e0ed2f70007c428b6e0ecc5dca9ddbe7cfb4
Chertenko <kchek816@gmail.com>
2024-01-16 23:11:05 +0300

    feat: unfinish modal

--- src/components/CreationScreens/StepScreens/ThirdStep.tsx
+++ src/components/CreationScreens/StepScreens/ThirdStep.tsx
@@ -1,15 +1,32 @@
-import React, { useEffect } from "react";
-import { View, Text, StyleSheet, Dimensions } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet, Dimensions, Modal, Pressable, ScrollView } from "react-native";
 import Icon from 'react-native-vector-icons/Ionicons'
 import { COLORS } from "../../../themes/COLORS";
 import { InputText } from "../../components/InputText";
 import { fetchRecipeCategories } from "../../../http/recipeCategoriesAPI";
+import { CheckBox } from "../../components/CheckBox";
+import { ModalV } from "../../components/Modal";
 
 export const ThirdStep = () => {
+    const [modalVisible, setModalVisible] = useState(false)
+    const [categories, setCategories] = useState<RecipeCategories[]>([])
 
+
+    const [selectedItems, setSelectedItems] = useState<string[]>([])
+
+    console.log(selectedItems)
+
+    const onCheckBoxClick = (itemId: string) => {
+        const isSelected = selectedItems.includes(itemId)
+        if(isSelected) {
+            setSelectedItems(prev => prev.filter(id => id !== itemId))
+        } else {
+            setSelectedItems(prev => [...prev, itemId])
+        }
+    }
 
     useEffect(() => {
-        fetchRecipeCategories().then(data => console.log(data)).catch(e => console.log(e))
+        fetchRecipeCategories().then(data => setCategories(data)).catch(e => console.log(e))
     }, [])
 
     return (
@@ -19,8 +36,14 @@ export const ThirdStep = () => {
         </View>
         <View style={styles.textInputContainer}>
             <View style={styles.inputContainer}>
-                <Icon name="happy-outline" size={24} color={COLORS.deepOrange}/>
-                <InputText keyboardType="numeric" placeholder="Количество порций" onChangeText={text => {}}/>
+                <Pressable onPress={() => setModalVisible(!modalVisible)}>
+                    <Text>Выберите категории</Text>
+                </Pressable>
+                <ModalV visible={modalVisible} onPress={() => setModalVisible(!modalVisible)}>           
+                    {categories.map((item) => 
+                        <CheckBox text={item.name} key={item._id} checked={selectedItems.includes(item._id)} onPress={() => onCheckBoxClick(item._id)}/>
+                    )}
+                </ModalV>
             </View>
             <View style={styles.inputContainer}>
                 <Icon name="stopwatch-outline" size={24} color={COLORS.deepOrange}/>
@@ -52,5 +75,36 @@ const styles = StyleSheet.create({
         flexDirection: 'row', 
         alignItems:'center', 
         columnGap: 10
+    },
+    centeredModal: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    modalView: {
+        backgroundColor: COLORS.white,
+        padding: 35, 
+        borderRadius: 15,
+        elevation: 2,
+        shadowOffset: {
+        width: 0,
+        height: 1,
+        },
+       shadowOpacity: 0.15,
+       shadowRadius: 2,
+    },
+    modalCategories: {
+        marginBottom: 10
+    },
+    modalHide: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        backgroundColor: COLORS.orange,
+        borderRadius: 5,
+    },
+    modalHideText: {
+        fontSize: 20,
+        fontFamily: 'Rubik-Regular',
+        color: COLORS.white
     }
 })
