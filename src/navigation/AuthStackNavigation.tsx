80b4f76dd584c91bb1629a641776832b014a8578
Chertenko <kchek816@gmail.com>
2024-03-08 15:48:29 +0300

    RegRenderSelect

--- src/navigation/AuthStackNavigation.tsx
+++ src/navigation/AuthStackNavigation.tsx
@@ -3,12 +3,14 @@ import React from 'react'
 import { Start } from '../components/StartComponents/Start'
 import { Login } from '../components/StartComponents/Login'
 import { RegGoal } from '../components/StartComponents/RegGoal'
+import { RegGenderSelect } from '../components/StartComponents/RegGenderSelect'
 
 
 export type AuthParamList = {
     StackHome: undefined
     Login: undefined
     RegGoal: undefined
+    RegGenderSelect: undefined
 }
 
 const nativeStackOption: NativeStackNavigationOptions = {
@@ -24,6 +26,7 @@ export const StartStackNavigation = () => {
             <Stack.Screen name='StackHome' component={Start} options={{headerShown: false}}/>
             <Stack.Screen name='Login' component={Login} />
             <Stack.Screen name = 'RegGoal' component={RegGoal} />
+            <Stack.Screen name='RegGenderSelect' component={RegGenderSelect} />
         </Stack.Navigator>
     )
 }
