07837574be2932948e54a5d573db83993da09c7a
Chertenko <kchek816@gmail.com>
2024-03-18 14:54:50 +0300

    new func

--- src/http/productAPI.ts
+++ src/http/productAPI.ts
@@ -4,3 +4,8 @@ export const searchProduct = async (text: string) => {
     const {data} = await $host.get(`api/product/search?search=${text}`)
     return data
 }
+
+export const getProduct = async (id: string, quantity: string) => {
+    let {data} = await $host.get(`api/product/${id}/${quantity}`)
+    return data
+}
