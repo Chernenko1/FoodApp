601e4536186117fd1af6703942a365dbcd3c9d65
Chertenko <kchek816@gmail.com>
2024-03-22 18:20:27 +0300

    -

--- src/components/HomeScreens/MealsComponents/MealList.tsx
+++ src/components/HomeScreens/MealsComponents/MealList.tsx
@@ -1,17 +1,20 @@
 import { ScrollView, StyleSheet, View } from "react-native"
 import { Meal } from "./Meal"
 import { useAppSelector } from "../../../store/hooks"
+import { MealContext } from "../Context/MealContext"
 
 export const MealList = () => {
     const {breakfast, dinner, snack, lunch} = useAppSelector(state => state.meals.meals)
 
     return (
-        <View style={styles.mainView}>
-            <Meal title="Завтрак" kcal={breakfast.calories} type="breakfast"/>
-            <Meal title="Обед" kcal={lunch.calories} type="lunch"/>
-            <Meal title="Ужин" kcal={dinner.calories} type="dinner"/>
-            <Meal title="Перекус" kcal={snack.calories} type="snack"/>
-        </View>
+        <MealContext.Provider value={''}>
+            <View style={styles.mainView}>
+                <Meal title="Завтрак" kcal={breakfast.calories} type="breakfast"/>
+                <Meal title="Обед" kcal={lunch.calories} type="lunch"/>
+                <Meal title="Ужин" kcal={dinner.calories} type="dinner"/>
+                <Meal title="Перекус" kcal={snack.calories} type="snack"/>
+            </View>
+        </MealContext.Provider>
     )
 }
 
