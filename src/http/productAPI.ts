47ac84082b859ec5ec3ac0e90b97f1f865ee8738
Chertenko <kchek816@gmail.com>
2024-05-02 19:17:25 +0300

    add: createProduct

--- src/http/productAPI.ts
+++ src/http/productAPI.ts
@@ -1,11 +1,16 @@
-import { $host } from ".";
+import {$host} from '.';
 
 export const searchProduct = async (text: string) => {
-    const {data} = await $host.get(`api/product/search?search=${text}`)
-    return data
-}
+  const {data} = await $host.get(`api/product/search?search=${text}`);
+  return data;
+};
 
 export const getProduct = async (id: string, quantity: string) => {
-    let {data} = await $host.get(`api/product/${id}/${quantity}`)
-    return data
-}
+  let {data} = await $host.get(`api/product/${id}/${quantity}`);
+  return data;
+};
+
+export const addProduct = async (product: Product) => {
+  const {data} = await $host.post(`api/product`, product);
+  return data;
+};
