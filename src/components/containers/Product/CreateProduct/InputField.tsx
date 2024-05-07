61e067ef40fa5ea0d743032d11485beeccf450eb
Chertenko <kchek816@gmail.com>
2024-05-07 21:45:35 +0300

    fix: styles

--- src/components/containers/Product/CreateProduct/InputField.tsx
+++ src/components/containers/Product/CreateProduct/InputField.tsx
@@ -1,17 +1,21 @@
 import {AppText} from 'components/common/AppText';
 import {StyleSheet, View} from 'react-native';
+import {useTheme} from '@react-navigation/native';
 
 interface IInputField {
   title: string;
+  ms?: string;
   children: React.ReactNode;
 }
-export const InputField = ({title, children}: IInputField) => {
+export const InputField = ({title, ms = 'г', children}: IInputField) => {
+  const {colors} = useTheme();
+
   return (
-    <View style={styles.inputContainer}>
+    <View style={[styles.inputContainer, {backgroundColor: colors.card}]}>
       <AppText style={styles.inputTitle}>{title}</AppText>
       <View style={styles.inputInfo}>
         {children}
-        <AppText style={styles.weightText}>г</AppText>
+        <AppText style={styles.weightText}>{ms}</AppText>
       </View>
     </View>
   );
