7404524136eb914ded45c503a3fd8ed72f06780c
Chertenko <kchek816@gmail.com>
2024-05-09 20:51:42 +0300

    add: FoodCard

--- src/navigation/Stacks/RecipesStack.tsx
+++ src/navigation/Stacks/RecipesStack.tsx
@@ -2,12 +2,15 @@ import {
   NativeStackNavigationOptions,
   createNativeStackNavigator,
 } from '@react-navigation/native-stack';
-import {Recipes} from 'components/containers/Recipes/Recipes';
 import React from 'react';
 
+import {FoodCard} from 'components/common/Cards/FoodCard/FoodCard';
+import {Recipe} from 'components/containers/Recipes/Recipe';
+import {Recipes} from 'components/containers/Recipes/Recipes';
+
 const nativeStackOption: NativeStackNavigationOptions = {
-  headerShown: false,
-  headerShadowVisible: false,
+  headerShown: true,
+  headerShadowVisible: true,
 };
 
 const Stack = createNativeStackNavigator<RecipesParamList>();
@@ -15,7 +18,17 @@ const Stack = createNativeStackNavigator<RecipesParamList>();
 export const RecipesStack = () => {
   return (
     <Stack.Navigator screenOptions={nativeStackOption}>
-      <Stack.Screen name="RecipesStack" component={Recipes} />
+      <Stack.Screen
+        name="RecipesStack"
+        component={Recipes}
+        options={{headerShown: false}}
+      />
+      <Stack.Screen
+        name="Recipe"
+        component={Recipe}
+        options={{headerShown: true, headerShadowVisible: true}}
+      />
+      <Stack.Screen name="FoodCard" component={FoodCard} />
     </Stack.Navigator>
   );
 };
