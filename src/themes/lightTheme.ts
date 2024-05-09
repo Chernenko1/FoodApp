bee292dd5a8e2ac3c6dc3e82cc00dd9403fc3e0a
Chertenko <kchek816@gmail.com>
2024-05-09 12:12:18 +0300

    fix: lightTheme

--- src/themes/lightTheme.ts
+++ src/themes/lightTheme.ts
@@ -1,11 +1,11 @@
 export const LightTheme = {
-    dark: false,
-    colors: {
-        background: 'rgb(242,242,242)',
-        card: 'rgb(255,255,255)',
-        text: 'rgb(0,0,0)',
-        border: 'rgb(0,0,0)',
-        primary: 'rgb(255,152,0)',
-        notification: 'rgb(255, 69, 58)',
-    }
-}
+  dark: false,
+  colors: {
+    card: 'rgb(242,242,242)',
+    background: 'rgb(255,255,255)',
+    text: 'rgb(0,0,0)',
+    border: 'rgb(0,0,0)',
+    primary: 'rgb(255,152,0)',
+    notification: 'rgb(255, 69, 58)',
+  },
+};
