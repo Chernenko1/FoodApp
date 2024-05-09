0da529912980b0a74b6c7e1da8cdb68b0e1c57ab
Chertenko <kchek816@gmail.com>
2024-05-09 20:48:54 +0300

    fix: date

--- src/store/slices/appSettings.ts
+++ src/store/slices/appSettings.ts
@@ -1,27 +1,27 @@
-import { createSlice } from "@reduxjs/toolkit";
-import { RootState } from "../store";
-import { formatDate } from "../../utils/formDate";
+import {createSlice} from '@reduxjs/toolkit';
+import {RootState} from '../store';
+import {formatDate} from '../../utils/formDate';
 
 interface IAppSettings {
-    date: string,
+  date: Date;
 }
 
 const initialState: IAppSettings = {
-    date: formatDate(new Date())
-}
+  date: new Date(),
+};
 
 export const appSettingsSlice = createSlice({
-    name: 'app',
-     initialState,
-     reducers: {
-        setAppDate(state, action) {
-            state.date = action.payload
-        }
-     }
-})
+  name: 'app',
+  initialState,
+  reducers: {
+    setAppDate(state, action) {
+      state.date = action.payload;
+    },
+  },
+});
 
-export const {setAppDate} = appSettingsSlice.actions
+export const {setAppDate} = appSettingsSlice.actions;
 
-export const selectAuth = (state: RootState) => state.app
+export const selectAuth = (state: RootState) => state.app;
 
-export default appSettingsSlice.reducer
+export default appSettingsSlice.reducer;
