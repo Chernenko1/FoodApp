45d99fcf8ebc6e92094ffaa5e79018e036010dba
Chertenko <kchek816@gmail.com>
2024-05-09 20:49:16 +0300

    fix: new fetch

--- src/services/apis/mealAPI.ts
+++ src/services/apis/mealAPI.ts
@@ -1,17 +1,21 @@
-import { combineSlices } from "@reduxjs/toolkit"
-import { $host } from "."
+import {$host} from '.';
 
-export const getMealData = async (id:string, mealType: string) => {
-    const {data} = await $host.get(`/api/meals/one/${id}/${mealType}`)
-    return data
-}
+export const getMealData = async (id: string, mealType: string) => {
+  const {data} = await $host.get(`/api/meals/one/${id}/${mealType}`);
+  return data;
+};
 
-export const addProductToMeal = async(productInfo: {}) => {
-    const {data} = await $host.post(`api/meals`, productInfo)
-    return data
-}
+export const addProductToMeal = async (productInfo: {}) => {
+  const {data} = await $host.post(`api/meals`, productInfo);
+  return data;
+};
 
-export const deleteProducFromMeal = async (productInfo: {}) => {    
-    const {data} = await $host.delete(`api/meals`, {data: productInfo})
-    return data
-}
+export const addFoodToMeal = async (foodInfo: upLoadObject) => {
+  const {data} = await $host.post(`api/meals`, foodInfo);
+  return data;
+};
+
+export const deleteProducFromMeal = async (productInfo: {}) => {
+  const {data} = await $host.delete(`api/meals`, {data: productInfo});
+  return data;
+};
