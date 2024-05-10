1494a937149d03630e6179e03c3fa05d7f7dd177
Chertenko <kchek816@gmail.com>
2024-05-10 19:23:04 +0300

    -

--- src/components/common/Inputs/TextInput.tsx
+++ src/components/common/Inputs/TextInput.tsx
@@ -2,6 +2,7 @@ import {useTheme} from '@react-navigation/native';
 import {StyleProp, TextInputProps, TextStyle, ViewStyle} from 'react-native';
 import {StyleSheet, TextInput, View} from 'react-native';
 import {COLORS} from '../../../themes/COLORS';
+import React, {forwardRef} from 'react';
 
 interface Props extends TextInputProps {
   value?: string;
@@ -11,44 +12,40 @@ interface Props extends TextInputProps {
   height?: number;
 }
 
-export const InputText = ({
-  value,
-  error,
-  style,
-  touched,
-  height = 40,
-  ...restProps
-}: Props) => {
-  const {colors} = useTheme();
+export const InputText = forwardRef<TextInput>(
+  ({value, error, style, touched, height = 40, ...restProps}: Props, ref) => {
+    const {colors} = useTheme();
 
-  const validationColor = !touched
-    ? COLORS.orange
-    : error
-    ? '#FF5A5F'
-    : COLORS.red;
+    const validationColor = touched
+      ? COLORS.orange
+      : error
+      ? '#FF5A5F'
+      : COLORS.red;
 
-  return (
-    <View style={[styles.mainView, {borderColor: validationColor, height}]}>
-      <View style={{}}>
-        <TextInput
-          value={value}
-          underlineColorAndroid="transparent"
-          placeholderTextColor={colors.text}
-          {...restProps}
-          style={[
-            {
-              padding: 2,
-              color: colors.text,
-              fontSize: 20,
-              borderColor: validationColor,
-            },
-            style,
-          ]}
-        />
+    return (
+      <View style={[styles.mainView, {borderColor: validationColor, height}]}>
+        <View style={{}}>
+          <TextInput
+            ref={ref}
+            value={value}
+            underlineColorAndroid="transparent"
+            placeholderTextColor={colors.text}
+            {...restProps}
+            style={[
+              {
+                padding: 2,
+                color: colors.text,
+                fontSize: 20,
+                borderColor: validationColor,
+              },
+              style,
+            ]}
+          />
+        </View>
       </View>
-    </View>
-  );
-};
+    );
+  },
+);
 
 const styles = StyleSheet.create({
   mainView: {
