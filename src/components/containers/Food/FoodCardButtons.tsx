042699045fd432e4f18e955e1cfe9799c740a654
Chertenko <kchek816@gmail.com>
2024-05-17 19:31:33 +0300

    del: funcs, add: callback

--- src/components/containers/Food/FoodCardButtons.tsx
+++ src/components/containers/Food/FoodCardButtons.tsx
@@ -1,60 +1,24 @@
-import {useNavigation} from '@react-navigation/native';
-import {
-  NativeStackNavigationProp,
-  NativeStackNavigatorProps,
-} from '@react-navigation/native-stack/lib/typescript/src/types';
 import {Button} from 'components/common/Buttons/Button';
 import {useState} from 'react';
 import {StyleSheet, View} from 'react-native';
-import {addFoodToMeal} from 'services/apis/mealAPI';
-import {useAppDispatch, useAppSelector} from 'store/hooks';
-import {setMeals} from 'store/slices/MealSlice';
 
 import {COLORS} from 'themes/COLORS';
 
 interface IFoodCardButtons {
   action: 'add' | 'update';
-  mealType: MealType;
-  id: string;
-  productType: 'recipe' | 'food';
-  weight: number;
-  nutrients: Nutrients;
+  disabled: boolean;
+  onPressAdd?: () => void;
+  onPressDelete?: () => void;
+  onPressUpdate?: () => void;
 }
-
-type Navigation = NativeStackNavigationProp<HomeParamList, 'FoodCard'>;
 
 export const FoodCardButtons = ({
   action,
-  productType,
-  mealType,
-  id,
-  weight,
-  nutrients,
+  disabled,
+  onPressAdd,
+  onPressDelete,
+  onPressUpdate,
 }: IFoodCardButtons) => {
-  const [disabled, setDisabled] = useState(false);
-
-  const date = useAppSelector(state => state.app.date);
-  const {_id} = useAppSelector(state => state.user.user);
-
-  const dispatch = useAppDispatch();
-  const navigation = useNavigation<Navigation>();
-
-  function addProduct() {
-    setDisabled(!disabled);
-    addFoodToMeal({
-      date,
-      mealType,
-      productType,
-      userId: _id,
-      data: {id, weight, nutrients},
-    })
-      .then(data => {
-        navigation.navigate('StackHome');
-        dispatch(setMeals(data));
-      })
-      .catch(e => console.log(e));
-  }
-
   return (
     <View style={styles.buttonView}>
       {action === 'add' ? (
@@ -64,7 +28,7 @@ export const FoodCardButtons = ({
             title="Добавить"
             color={COLORS.orange}
             textColor={COLORS.white}
-            onPress={addProduct}
+            onPress={onPressAdd}
           />
         </View>
       ) : (
@@ -72,16 +36,18 @@ export const FoodCardButtons = ({
           <Button
             style={styles.button}
             title="Удалить"
+            disabled={disabled}
             color={COLORS.red}
             textColor={COLORS.white}
-            // onPress={deleteProduct}
+            onPress={onPressDelete}
           />
           <Button
             style={styles.button}
             title="Сохранить"
+            disabled={disabled}
             color={COLORS.orange}
             textColor={COLORS.white}
-            // onPress={updateProduct}
+            onPress={onPressUpdate}
           />
         </>
       )}
