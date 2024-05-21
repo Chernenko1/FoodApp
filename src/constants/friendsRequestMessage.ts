94910cc8dde94ba6faed63e2ae504d8ecc5b5fbc
Chertenko <kchek816@gmail.com>
2024-05-21 19:29:22 +0300

    feat: constant

---
+++ src/constants/friendsRequestMessage.ts
@@ -0,0 +1,5 @@
+export enum friendsRequestMessage {
+  USER_HIDE_DATA_FROM_ALL_USERS = 'Пользователь скрыл данные от всех пользователей',
+  USER_HIDE_DATA_FROM_UNFRIENDS = 'Пользователь открыл данные только для друзей',
+  ALL_USER_DATA = '',
+}
