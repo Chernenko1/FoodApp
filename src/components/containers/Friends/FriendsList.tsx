4699168180db1d8849fbbd69cab325faf350db9b
Chertenko <kchek816@gmail.com>
2024-05-21 19:28:07 +0300

    add: FriendsList

---
+++ src/components/containers/Friends/FriendsList.tsx
@@ -0,0 +1,54 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import {AppText} from 'components/common/AppText';
+import {TextButton} from 'components/common/Buttons/TextButton';
+import {SearchInput} from 'components/common/SearchInput';
+import {FlatList, StyleSheet, View} from 'react-native';
+import {useAppSelector} from 'store/hooks';
+
+type Navigation = NativeStackScreenProps<FriendsParamList, 'FriendsList'>;
+
+export const FriendsList = ({navigation, route}: Navigation) => {
+  const {friends} = route.params;
+
+  const {_id} = useAppSelector(state => state.user.user);
+
+  function navigateToFriendPage(id: string) {
+    navigation.push('FriendPage', {id});
+  }
+
+  return (
+    <View style={styles.mainContainer}>
+      <SearchInput placeholder="Поиск пользователей" />
+      <AppText style={styles.headerText} fontWeight="bold" size={38}>
+        Друзья:
+      </AppText>
+      <View style={styles.listView}>
+        <FlatList
+          data={friends}
+          keyExtractor={item => item._id}
+          renderItem={({item}) => (
+            <TextButton
+              title={item.username}
+              size={24}
+              onPress={() => navigateToFriendPage(item._id)}
+            />
+          )}
+        />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainContainer: {
+    paddingHorizontal: 15,
+    paddingTop: 10,
+  },
+  listView: {
+    // marginTop: 10,
+  },
+  headerText: {
+    paddingVertical: 10,
+    textAlign: 'center',
+  },
+});
