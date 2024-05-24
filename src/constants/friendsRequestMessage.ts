166bbd6e954241da5c4bf289cf976f1cebb3f766
Chertenko <kchek816@gmail.com>
2024-05-24 15:29:53 +0300

    add: new errors

--- src/constants/friendsRequestMessage.ts
+++ src/constants/friendsRequestMessage.ts
@@ -1,5 +1,7 @@
 export enum friendsRequestMessage {
   USER_HIDE_DATA_FROM_ALL_USERS = 'Пользователь скрыл данные от всех пользователей',
   USER_HIDE_DATA_FROM_UNFRIENDS = 'Пользователь открыл данные только для друзей',
+  USER_HIDE_WEIGHT = 'Пользователь скрыл данные о весе',
+  USER_HIDE_CPFC = 'Пользователь скрыл данные о КБЖУ',
   ALL_USER_DATA = '',
 }
