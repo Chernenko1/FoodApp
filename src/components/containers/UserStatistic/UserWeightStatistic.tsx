8a0a7b854b68fba676d534d4684736257b8bc4c5
Chertenko <kchek816@gmail.com>
2024-05-28 20:32:49 +0300

    feat: UserWeigthStatistic

---
+++ src/components/containers/UserStatistic/UserWeightStatistic.tsx
@@ -0,0 +1,12 @@
+import {AppText} from 'components/common/AppText';
+import {View} from 'react-native';
+
+export const UserWeightStatistic = () => {
+  return (
+    <View>
+      <View>
+        <AppText>Ваши изменения веса</AppText>
+      </View>
+    </View>
+  );
+};
