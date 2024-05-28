a319752cc7e43fe6fd070d9b04ac16c42bad7932
Chertenko <kchek816@gmail.com>
2024-05-28 20:31:14 +0300

    feat: FriendWeigth

---
+++ src/components/containers/Friends/FriendWeight.tsx
@@ -0,0 +1,70 @@
+import {AppText} from 'components/common/AppText';
+import {CircleProgressBar} from 'components/common/Charts/CircleProgressBar';
+import {StyleSheet, View} from 'react-native';
+
+interface IUserWeightStatistic {
+  startWeigth: number;
+  currentWeight: number;
+  targetWeight: number;
+}
+
+export const FriendWeight = ({
+  currentWeight,
+  startWeigth,
+  targetWeight,
+}: IUserWeightStatistic) => {
+  let progress =
+    ((currentWeight - startWeigth) / (targetWeight - startWeigth)) * 100;
+  progress = progress < 0 ? progress * -1 : progress;
+
+  return (
+    <View style={styles.mainView}>
+      <AppText style={styles.headerText} fontWeight="medium" size={30}>
+        Вес
+      </AppText>
+      <View style={styles.weightView}>
+        <View style={styles.weightInfo}>
+          <AppText>Начальный вес: {startWeigth}кг</AppText>
+          <AppText>Цель: {targetWeight}кг</AppText>
+        </View>
+        <View>
+          <View style={styles.progressView}>
+            <AppText size={50}>{currentWeight}</AppText>
+            <AppText
+              style={{position: 'relative', top: -20}}
+              size={30}
+              fontWeight="medium">
+              кг
+            </AppText>
+          </View>
+          <CircleProgressBar progress={progress + 1} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  mainView: {
+    marginTop: 10,
+    marginBottom: 10,
+  },
+  weightView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexWrap: 'wrap',
+  },
+  weightInfo: {
+    width: '50%',
+  },
+  progressView: {
+    position: 'absolute',
+    top: 40,
+    right: 60,
+    alignItems: 'center',
+  },
+  headerText: {
+    textAlign: 'center',
+  },
+});
