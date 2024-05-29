89409a8a14cac4c380c90f9594542dfefbdaf7d4
Chertenko <kchek816@gmail.com>
2024-05-29 20:38:34 +0300

    add: errorText

--- src/components/containers/Profile/UserComponents/UserDetailsModal/styles.ts
+++ src/components/containers/Profile/UserComponents/UserDetailsModal/styles.ts
@@ -1,19 +1,22 @@
-import { StyleSheet } from "react-native";
+import {StyleSheet} from 'react-native';
 
 export const styles = StyleSheet.create({
-    mainView: {
-        alignItems: 'center',
-        paddingVertical: 10
-    },
-    inputView: {
-        flexDirection: 'row',
-        alignItems: 'center'
-    },
-    titleText: {
-        fontSize: 40,
-        textAlign: 'center'
-    },
-    input: {
-        fontSize: 35,
-    }
-})
+  mainView: {
+    alignItems: 'center',
+    paddingVertical: 10,
+  },
+  inputView: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  titleText: {
+    fontSize: 40,
+    textAlign: 'center',
+  },
+  input: {
+    fontSize: 35,
+  },
+  errorText: {
+    color: 'red',
+  },
+});
