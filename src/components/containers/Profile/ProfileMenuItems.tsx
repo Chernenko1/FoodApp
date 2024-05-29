f8a4ec98abfc7d301fccc9d33046afa67a38a79a
Chertenko <kchek816@gmail.com>
2024-05-29 20:38:44 +0300

    add: exitButton

--- src/components/containers/Profile/ProfileMenuItems.tsx
+++ src/components/containers/Profile/ProfileMenuItems.tsx
@@ -1,30 +1,50 @@
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {StyleSheet, View} from 'react-native';
+import {useAppDispatch} from 'store/hooks';
 
 import {MenuItem} from './MenuItem';
+import {COLORS} from 'themes/COLORS';
+import {setUser} from 'store/slices/userSlice';
 
-type Navigation = NativeStackNavigationProp<SettingsParamList, 'Settings'>;
+type Navigation = NativeStackNavigationProp<SettingsParamList, 'SettingsStack'>;
 
 export const ProfileMenuItems = () => {
   const navigation: Navigation = useNavigation();
 
+  const dispatch = useAppDispatch();
+
+  function unLogin() {
+    dispatch(setUser({}));
+  }
+
   return (
     <View style={styles.mainView}>
-      <MenuItem
-        title="Персональные данные"
-        icon="person-outline"
-        onPress={() => navigation.push('UserDetails')}
-      />
-      <MenuItem
-        title="Настройка KБЖУ"
-        icon="pie-chart-outline"
-        onPress={() => navigation.navigate('KBFUSettings')}
-      />
+      <View>
+        <MenuItem
+          title="Персональные данные"
+          icon="person-outline"
+          onPress={() => navigation.push('UserDetails')}
+        />
+        <MenuItem
+          title="Настройка KБЖУ"
+          icon="pie-chart-outline"
+          onPress={() => navigation.navigate('KBFUSettings')}
+        />
+        <MenuItem title="Выйти" icon="exit-outline" onPress={unLogin} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
-  mainView: {},
+  mainView: {
+    flex: 1,
+    paddingBottom: 65,
+    justifyContent: 'space-between',
+  },
+  exitButton: {
+    textAlign: 'center',
+    color: COLORS.red,
+  },
 });
