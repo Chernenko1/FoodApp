1c91d6bedd7f92a52dd336bfa1e79c6fa00945dc
Chertenko <kchek816@gmail.com>
2024-05-29 20:40:26 +0300

    fix: func

--- src/components/containers/Profile/UserComponents/UserDetailsModal/UserWeightModal.tsx
+++ src/components/containers/Profile/UserComponents/UserDetailsModal/UserWeightModal.tsx
@@ -6,8 +6,9 @@ import {styles} from './styles';
 import {useAppDispatch, useAppSelector} from '../../../../../store/hooks';
 import {ModalV} from '../../../../common/Modals/Modal';
 import {AppText} from '../../../../common/AppText';
-import {updateUserDetails} from '../../../../../services/apis/userAPI';
-import {updateDetails} from '../../../../../store/slices/userSlice';
+import {userApi} from '../../../../../services/apis/userAPI';
+import {addWeight, updateDetails} from '../../../../../store/slices/userSlice';
+import {formatDate} from 'utils/formDate';
 
 interface Props {
   text: string;
@@ -21,12 +22,19 @@ export const UserWeightModal = ({text, visible, closeModal}: Props) => {
   const dispatch = useAppDispatch();
   const {colors} = useTheme();
 
+  const {error, loading, addNewWeight} = userApi();
+
   const {_id} = useAppSelector(state => state.user.user);
 
   function handlePress() {
-    updateUserDetails({id: _id, type: 'weight', data: value});
-    dispatch(updateDetails({type: 'weight', value}));
-    closeModal();
+    addNewWeight(_id, +value);
+    if (!loading && !error) {
+      dispatch(updateDetails({type: 'weight', value}));
+      dispatch(addWeight({day: formatDate(new Date()), weight: +value}));
+      closeModal();
+    } else if (!loading && error) {
+      console.log(error);
+    }
   }
 
   return (
@@ -45,6 +53,12 @@ export const UserWeightModal = ({text, visible, closeModal}: Props) => {
           />
           <Text style={[styles.input, {color: colors.text}]}>кг</Text>
         </View>
+        {error && (
+          <AppText style={styles.errorText}>
+            Что-то пошло не так, попробуйте снова, если ошибка не исчезла
+            напишите в поддержку
+          </AppText>
+        )}
       </View>
     </ModalV>
   );
