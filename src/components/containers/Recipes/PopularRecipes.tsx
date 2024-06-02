0366e71d346c271659e132b13525950c8c73ace9
Chertenko <kchek816@gmail.com>
2024-06-02 15:41:59 +0300

    fix

--- src/components/containers/Recipes/PopularRecipes.tsx
+++ src/components/containers/Recipes/PopularRecipes.tsx
@@ -1,59 +1,77 @@
 import {useNavigation} from '@react-navigation/native';
 import {NativeStackNavigationProp} from '@react-navigation/native-stack';
 import {AppText} from 'components/common/AppText';
+import {TextButton} from 'components/common/Buttons/TextButton';
 import {useEffect, useState} from 'react';
 import {Image, Pressable, StyleSheet, View} from 'react-native';
 
 import Icon from 'react-native-vector-icons/Ionicons';
 import {fetchRecipes, recipeApi} from 'services/apis/recipeAPI';
-import {useAppSelector} from 'store/hooks';
+import {useAppDispatch, useAppSelector} from 'store/hooks';
+import {setFavouriteRecipes} from 'store/slices/recipesSlice';
 import {COLORS} from 'themes/COLORS';
 
 type Navigation = NativeStackNavigationProp<RecipesParamList, 'RecipesStack'>;
 
 export const PopularRecipes = () => {
-  const [recipes, setRecipes] = useState<Recipe[]>();
-
   const navigation = useNavigation<Navigation>();
+  const dispatch = useAppDispatch();
   const userId = useAppSelector(state => state.user.user._id);
+  const recipes = useAppSelector(state => state.recipes.favouriteRecipes);
 
   const {getFavourite, isError, isLoading, error} = recipeApi();
 
   useEffect(() => {
-    getFavourite(userId).then(data => console.log(data));
+    getFavourite(userId).then(data => dispatch(setFavouriteRecipes(data)));
   }, []);
 
   function navigateToRecipe(_id: string) {
     navigation.push('Recipe', {_id});
   }
 
+  function navigateToAllRecipes() {
+    navigation.push('AllRecipes', {func: 'fav', title: 'Избранные рецепты'});
+  }
+
   return (
     <View style={styles.container}>
-      <AppText>Избранные рецепты</AppText>
-      <View style={styles.recipeContainer}>
-        {recipes?.slice(0, 6).map(item => (
-          <Pressable
-            key={item._id}
-            style={styles.recipeView}
-            onPress={() => navigateToRecipe(item._id)}>
-            <Image
-              source={require('../../../../assets/images/noImage.jpg')}
-              style={styles.image}
-            />
-            <View style={styles.descriptionView}>
-              <AppText fontWeight="medium" size={24} numberOfLines={1}>
-                {item.name}
-              </AppText>
-              <AppText
-                style={styles.textDescription}
-                numberOfLines={2}
-                size={18}>
-                {item.description}
-              </AppText>
-            </View>
-          </Pressable>
-        ))}
+      <View style={styles.headerView}>
+        <AppText size={24} fontWeight="medium">
+          Избранные рецепты
+        </AppText>
+        <TextButton
+          title="Все рецепты ->"
+          color={COLORS.orange}
+          size={18}
+          onPress={navigateToAllRecipes}
+        />
       </View>
+      {recipes.length !== 0 && (
+        <View style={styles.recipeContainer}>
+          {recipes.slice(0, 2).map(item => (
+            <Pressable
+              key={item._id}
+              style={styles.recipeView}
+              onPress={() => navigateToRecipe(item._id)}>
+              <Image
+                source={require('../../../../assets/images/noImage.jpg')}
+                style={styles.image}
+              />
+              <View style={styles.descriptionView}>
+                <AppText fontWeight="medium" size={24} numberOfLines={1}>
+                  {item.name}
+                </AppText>
+                <AppText
+                  style={styles.textDescription}
+                  numberOfLines={2}
+                  size={18}>
+                  {item.description}
+                </AppText>
+              </View>
+            </Pressable>
+          ))}
+        </View>
+      )}
     </View>
   );
 };
@@ -83,4 +101,9 @@ const styles = StyleSheet.create({
     columnGap: 5,
   },
   textDescription: {},
+  headerView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
 });
